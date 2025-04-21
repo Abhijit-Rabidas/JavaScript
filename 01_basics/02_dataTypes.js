2e3bf0f126b9ec3f35d917222338537d18a881cf
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2025-04-21 11:46:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // we are using nodejs, not browsers
+// alert(3+3)
+
+// console.log(3+3) console.log("Hello") // Error
+// console.log(3+3); console.log("Hello") // correct
+
+// prefered
+console.log(3+3) 
+console.log("Hello")
+
+// code readability should be high
+
+let name = "Abhijit"
+let age = 18
+let isLoggedIn = false
+
+/* Primitive Data Types */
+
+// number => 2 to power 53
+// bigint
+// string => " ", or ' '
+// boolean => true/false
+// null => standalone value // representation of empty value
+// undefined => value not assigned
+// symbol =>  unique 
+
+// ----------------------------
+
+// object
+
+console.log(typeof "Abhijit");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
