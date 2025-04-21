ba7857737d9e301a2d4c6c05b03aeefe676cff30
Abhijit Rabidas <aaacs.hackers101@gmail.com>
2025-04-21 12:10:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,39 @@
+//let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Abhijit"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+//let isLoggedIn = 1
+//let isLoggedIn = ""
+let isLoggedIn = "Abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Abhi" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
